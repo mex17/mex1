bdb359923f6621851166c08d6e3204883ddd7a98
mex17 <ishimwestalon17@gmail.com>
2018-12-04 12:45:12 +0200

    calc

---
+++ ConsoleApplication5/ConsoleApplication5/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Calculate
+    {
+        public double ADD(int NUM1,int NUM2)
+        {
+            return NUM1 +NUM2;
+        }
+         public double SUBSTRACT(int NUM1,int NUM2)
+        {
+            return NUM1 - NUM2;
+        }
+         public double MULTIPLY(int NUM1, int NUM2)
+         {
+             return NUM1 * NUM2;
+         }
+         public double DIVIDE(int NUM1,int NUM2)
+        {
+            return NUM1 / NUM2;
+        }
+    }
+}
