179cf137c19eb792b340aa1764d64ba9584f2f93
mex17 <ishimwestalon17@gmail.com>
2019-03-07 10:01:57 +0200

    csharp

---
+++ csharp1.db/csharp1.db/Migrations/20190307074950_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace csharp1.db.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    MovieId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    title = table.Column<string>(nullable: true),
+                    year = table.Column<string>(nullable: true),
+                    category = table.Column<string>(nullable: true),
+                    budget = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.MovieId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
---
+++ csharp1.db/csharp1.db/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MovieApp.NewDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new MovieContext())
+            {
+                Console.WriteLine("enter movie title");
+                string title = Console.ReadLine();
+                Console.WriteLine("enter movie released year");
+                string year = Console.ReadLine();
+                Console.WriteLine("enter movie category");
+                string category = Console.ReadLine();
+                Console.WriteLine("enter movie budget");
+                string budget = Console.ReadLine();
+                db.Movies.Add(new Movietbl { title = title });
+                db.Movies.Add(new Movietbl { year = year });
+                db.Movies.Add(new Movietbl { category = category });
+                db.Movies.Add(new Movietbl { budget = budget });
+                var count = db.SaveChanges();
+                Console.WriteLine("{0} records saved to database", count);
+
+                Console.WriteLine();
+                Console.WriteLine("All blogs in database:");
+                foreach (var Movietbl in db.Movies)
+                {
+                    Console.WriteLine(" - {0}", Movietbl.title, Movietbl.year, Movietbl.category, Movietbl.budget);
+                }
+            }
+        }
+    }
+}
